095f890525bd7d1a940d0136cfd6594422cae34c
александр <chika431sis@yandex.ru>
2023-10-20 23:00:20 +0500

    task

---
+++ task 69/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int Exponentiation(int numA, int numB)
+{
+    if(numB == 0) return 1;
+    return numA * Exponentiation(numA, numB-1);
+    
+}
+
+Console.WriteLine("Введите натуральное число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Exponentiation(numberA, numberB);
+Console.Write($"A = {numberA}, B = {numberB} -> ");
+Console.WriteLine(Exponentiation(numberA,numberB));
